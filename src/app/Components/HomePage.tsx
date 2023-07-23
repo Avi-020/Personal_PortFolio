7f2318441903d8e663a0e6584e0a52cd252407d7
Avi-020 <avipawar020@gmail.com>
2023-07-23 17:30:50 +0530

    changes

--- src/app/Components/HomePage.tsx
+++ src/app/Components/HomePage.tsx
@@ -1,12 +1,56 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import styles from "../CSS/HomePage.module.css";
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import TaskOutlinedIcon from '@mui/icons-material/TaskOutlined';
+
 
 function HomePage() {
   return (
-   <>
-   <h1>personal portfolio</h1>
-   
-   </>
-  )
+    <>
+      <div className={styles.sidebar}>
+        <div
+          style={{ width: "100%", backgroundColor: "", textAlign: "center" }}
+        >
+          <Image
+            src="/images/myimg.png"
+            width={110}
+            height={110}
+            alt="Picture of the author"
+            style={{ borderRadius: "100%", marginTop:"2vh" }}
+          />
+          <div className="socialMedia" style={{width:"100%",height:"4vh",backgroundColor:"red",marginBottom:"2vh"}}>
+          {/* <i className="fab fa-facebook-f fa-2x" style={{color: "#3b5998"}}></i> */}
+        
+
+          </div>
+        </div>
+        <a className="active" href="#home">
+          <HomeOutlinedIcon style={{marginRight:"1vh"}}/>
+          Home
+        </a>
+
+        <a href="#Resume"><DescriptionOutlinedIcon style={{marginRight:"1vh"}}/>Resume</a>
+        <a href="#Projects"> <TaskOutlinedIcon style={{marginRight:"1vh"}}/>Projects</a>
+        <a href="#Contact"> <ContactsOutlinedIcon style={{marginRight:"1vh"}}/>Contact</a>
+      </div>
+
+      <div className={styles.content} style={{backgroundImage:"url('/images/myimg.png')",backgroundSize:"cover"}}>
+        <h2>Responsive Sidebar Example</h2>
+        <p>
+          This example use media queries to transform the sidebar to a top
+          navigation bar when the screen size is 700px or less.
+        </p>
+        <p>
+          We have also added a media query for screens that are 400px or less,
+          which will vertically stack and center the navigation links.
+        </p>
+        <h3>Resize the browser window to see the effect.</h3>
+      </div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css'
+import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
+
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
